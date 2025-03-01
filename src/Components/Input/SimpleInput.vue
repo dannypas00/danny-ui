0f01e16b8ec0b8161ab3b40a34f9e9a886f83cf8
dannypas00 <github@dannypas.nl>
2025-03-01 09:05:39 +0100

    Fix input padding

--- src/Components/Input/SimpleInput.vue
+++ src/Components/Input/SimpleInput.vue
@@ -18,7 +18,7 @@
         :autocomplete
         :required
         :aria-invalid="error ? 'true' : 'false'"
-        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
+        class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand sm:text-sm sm:leading-6"
       />
       <span v-if="error" class="text-sm text-red-500">
         {{ error }}
